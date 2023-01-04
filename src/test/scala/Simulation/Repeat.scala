fd91fef722b61f717638c218baf8ec6ae7e77c24
Sabyasachi Nag <bbng731@gmail.com>
2023-01-04 19:38:39 +0530

    third commit

--- src/test/scala/Simulation/Repeat.scala
+++ src/test/scala/Simulation/Repeat.scala
@@ -3,11 +3,11 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 class Repeat extends Simulation{
 
-  val httpProtocol=http.baseUrl("https://gorest.co.in").acceptHeader("application/json")
+  val httpProtocol=http.baseUrl("https://www.videogamedb.uk/api").acceptHeader("application/json")
 
   def getSpecificUser()={
     repeat(5){
-      exec(http("Get specific user").get("/public/v2/users/134").check(status.in(200,304)))
+      exec(http("Get specific user").get("/videogame/1").check(status.in(200,304)))
 
     }
 
